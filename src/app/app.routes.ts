9b802c490e429c7057348eca8eb5b1ead4735a9d
kevinalarcon-sswh <kevin.alarcon@sunsetswh.com>
2026-09-07 13:23:07 -0500

    Update index.html language to Spanish, modify title to 'Spotify', enhance global styles in styles.css, and implement authentication routing with guards in app.routes.ts. Clean up app component and remove unused .gitkeep files.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { guestGuard } from './core/guards/guest.guard';
+import { Login } from './features/auth/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    component: Login,
+  },
+  {
+    path: 'home',
+    canActivate: [authGuard],
+    loadComponent: () => import('./features/home/home').then((m) => m.Home),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '**', redirectTo: 'login' },
+];
